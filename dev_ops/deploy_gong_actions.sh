0c934cdf1c74eb8deb23796827a95e7905e302bc
DhammaPamoda <tech@pamoda.dhamma.org>
2025-11-15 17:18:51 +0200

    Enhance deployment scripts: added support for specifying backend and frontend branch names in deploy_gong_actions.sh and updated related scripts to pass these parameters, improving flexibility in deployment.

--- dev_ops/deploy_gong_actions.sh
+++ dev_ops/deploy_gong_actions.sh
@@ -5,6 +5,8 @@ set -x
 USER=$1
 USER_PASS=$2
 IS_DOCKER=$3
+GONG_BE_BRANCH=$4
+GONG_FE_BRANCH=$5
 
 BASE_DIR="/home/${USER}/projects"
 GONG_DEV_OPS_DIR="${BASE_DIR}/gong_dev_ops"
@@ -21,9 +23,40 @@ cd "${BASE_DIR}/"
 
 export HISTIGNORE='*sudo -S*'
 
+# Function to validate branch exists on remote
+validate_branch() {
+  local repo_url=$1
+  local branch_name=$2
+  
+  if [ -z "$branch_name" ]; then
+    return 0  # Empty branch name means use default
+  fi
+  
+  if git ls-remote --heads "$repo_url" "$branch_name" | grep -q "refs/heads/$branch_name"; then
+    return 0  # Branch exists
+  else
+    echo "Error: Branch '$branch_name' does not exist on remote repository $repo_url"
+    return 1  # Branch does not exist
+  fi
+}
+
 # Getting the FE and BE
-git clone https://github.com/DhammaPamoda/Gong-be.git
-git clone https://github.com/DhammaPamoda/Gong_fe.git
+GONG_BE_REPO="https://github.com/DhammaPamoda/Gong-be.git"
+GONG_FE_REPO="https://github.com/DhammaPamoda/Gong_fe.git"
+
+if [ -n "${GONG_BE_BRANCH}" ]; then
+  validate_branch "${GONG_BE_REPO}" "${GONG_BE_BRANCH}" || exit 1
+  git clone -b "${GONG_BE_BRANCH}" "${GONG_BE_REPO}"
+else
+  git clone "${GONG_BE_REPO}"
+fi
+
+if [ -n "${GONG_FE_BRANCH}" ]; then
+  validate_branch "${GONG_FE_REPO}" "${GONG_FE_BRANCH}" || exit 1
+  git clone -b "${GONG_FE_BRANCH}" "${GONG_FE_REPO}"
+else
+  git clone "${GONG_FE_REPO}"
+fi
 
 # Populating the dev_ops
 cp -rf "${BASE_DIR}/Gong-be/dev_ops" "${GONG_DEV_OPS_DIR}/"
--- dev_ops/docker_init.sh
+++ dev_ops/docker_init.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+USER=$1
+USER_PASS=$2
+IS_DOCKER=$3
+GONG_BE_BRANCH=$4
+GONG_FE_BRANCH=$5
+
 git clone https://github.com/DhammaPamoda/Gong-be.git ~/Gong-be
 cp ~/Gong-be/dev_ops/deploy_gong.sh .
 cp ~/Gong-be/dev_ops/deploy_gong_actions.sh .
 rm -rf Gong-be
-sudo -S chmod +x ./deploy_gong.sh <<< $2
-sudo -S chmod +x ./deploy_gong_actions.sh <<< $2 
-./deploy_gong.sh $1 $2 $3
+sudo -S chmod +x ./deploy_gong.sh <<< "${USER_PASS}"
+sudo -S chmod +x ./deploy_gong_actions.sh <<< "${USER_PASS}" 
+./deploy_gong.sh "${USER}" "${USER_PASS}" "${IS_DOCKER}" "${GONG_BE_BRANCH}" "${GONG_FE_BRANCH}"
 bash
